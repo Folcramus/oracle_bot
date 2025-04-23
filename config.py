771669227eba2fefd2996cd617cb2f49b4dc8580
D1n0R <120053083+D1n0R@users.noreply.github.com>
2025-04-23 12:00:12 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-#BOT_TOKEN = os.getenv("BOT_TOKEN")
-#BOT_TOKEN = os.getenv("8166458477:AAEH7tgkTBOsKSBjhDMgc329XYXfxoxUxuY")
-BOT_TOKEN = "8166458477:AAEH7tgkTBOsKSBjhDMgc329XYXfxoxUxuY"
-PROVIDER_TOKEN = ""
